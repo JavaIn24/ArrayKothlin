06a46d94a01d994e79ee47e9e952c8ba020a4b3c
JavaIn24 <yaracrut245@gmail.com>
2024-08-21 13:53:13 +0500

    Update Main.kt

--- src/Task4/Main.kt
+++ src/Task4/Main.kt
@@ -1,2 +1,10 @@
 package Task4
-
+fun main () {
+    val n = 10
+    val array = IntArray(n)
+    array[0] = 2
+    for (i in 1..<n) {
+        array[i] = array[i - 1] * 2
+    }
+    println(array.contentToString())
+}
