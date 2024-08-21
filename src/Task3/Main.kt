2c900ec4e68df4b7a88753205a9d2097ea197aff
JavaIn24 <yaracrut245@gmail.com>
2024-08-21 13:52:52 +0500

    Update Main.kt

--- src/Task3/Main.kt
+++ src/Task3/Main.kt
@@ -1,2 +1,13 @@
 package Task3
+fun main (){
+    var iter = 0 // Переменная для подсчета количества итерации
+    var array = Array<Int>(10){a -> a + 1 }
+    for (i in array){
+        if (i % 2 == 0){
+            iter += 1
+            println(i)
 
+        }
+    }
+    println("Количество четных элементов " +
+            "в исходном массиве равно $iter")
